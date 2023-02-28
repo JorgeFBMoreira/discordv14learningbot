9de76ea496365e54edf8c943e465e752ca763f1b
JorgeFBMoreira <jorgemoreira.trabalho@gmail.com>
2023-02-28 19:08:15 +0000

    MongoDB Database added

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
-//import { Client, GatewayIntentBits, Partials, Collection } from 'discord.js';
-//import { config } from 'dotenv'; config(); 
 const { Client, GatewayIntentBits, Partials, Collection } = require('discord.js'); 
 const { config } = require('dotenv'); config();
 
-const { BOT_TOKEN, CLIENT_ID, GUILD_ID } = process.env;
+const { BOT_TOKEN, CLIENT_ID, GUILD_ID, MONGODB_URL } = process.env;
 const { Guilds, GuildMembers, GuildMessages } = GatewayIntentBits;
 const { User, Message, GuildMember, ThreadMember } = Partials;
 
@@ -24,9 +22,13 @@ const client = new Client({
 });
 
 const { loadEvents } = require('./Handlers/eventHandler.js'); 
-
 client.events = new Collection();
 client.commands = new Collection();
+
+const { connect } = require('mongoose');
+connect(MONGODB_URL, {
+}).then(() => console.log("The client is now connected to the database"));
+
 loadEvents(client);
 
 
